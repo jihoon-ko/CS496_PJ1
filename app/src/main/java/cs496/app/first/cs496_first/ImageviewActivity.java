f2d0cfd860c0d2513c5e44507062f7dbe8a6ac65
Jihoon Ko <jihoonko@kaist.ac.kr>
2017-07-01 22:41:17 +0900

    hide notification bar

--- app/src/main/java/cs496/app/first/cs496_first/ImageviewActivity.java
+++ app/src/main/java/cs496/app/first/cs496_first/ImageviewActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.github.chrisbanes.photoview.PhotoView;
 
@@ -12,6 +13,7 @@ public class ImageviewActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_imageview);
         Intent intent = getIntent();
         PhotoView photoView = (PhotoView) findViewById(R.id.detailPhotoview);
